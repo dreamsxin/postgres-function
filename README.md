f14fab1e8a75b138abff67980b76fb4fe8b98008
Dreamsxin <dreamsxin@126.com>
2014-12-10 14:18:06 +0800

    Updated README.md

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 postgres-function
 =================
+
+```shell
+git clone https://github.com/dreamsxin/postgres-function.git
+cd postgres-function
+make && sudo make install
+```
+
+```sql
+CREATE EXTENSION mongo;
+
+SELECT mongo_save(
+    'mongodb://127.0.0.1:27017',
+    'file',
+    'images',
+    '{"postgresql":"fine"}',
+    '{"name":"name1"}'
+);
+
+SELECT mongo_find(
+    'mongodb://127.0.0.1:27017',
+    'file',
+    'images',
+    '{"name":"name1"}'
+)->'postgresql'
+```
--- example.sql
+++ example.sql
@@ -2,23 +2,27 @@ SELECT mongo_find(
     'mongodb://127.0.0.1:27017',
     'file',
     'images',
-    '{"name":"name1"}')->'_id'->'$oid';
+    '{"name":"name1"}'
+)->'_id'->'$oid';
 
 SELECT mongo_find(
     'mongodb://127.0.0.1:27017',
     'file',
     'images',
-    '{"name":"name1"}')->'name';
+    '{"name":"name1"}'
+)->'name';
     
 SELECT mongo_save(
     'mongodb://127.0.0.1:27017',
     'file',
     'images',
     '{"postgresql":"fine"}',
-    '{"name":"name1"}');
+    '{"name":"name1"}'
+);
 
 SELECT mongo_find(
     'mongodb://127.0.0.1:27017',
     'file',
     'images',
-    '{"name":"name1"}')->'postgresql'
+    '{"name":"name1"}'
+)->'postgresql'
